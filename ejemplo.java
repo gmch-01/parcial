9cb06e4e73bec442f0fc32563cafdbb6cae291e9
Gonzalo Cuevas <manuelhinojosagc@gmail.com>
2020-04-23 09:02:08 -0400

    Archivo Original

---
+++ ejemplo.java
@@ -0,0 +1,21 @@
+public class pc {
+    /**
+    * @paras args
+     */
+    public static void main(String [] args ) {
+        // TADO Autogerenated method stab 
+
+        int matriz [][] = new int [5][5];
+
+        for (int i = 1; i=5; i++) {
+            System.out.println();
+            for (int j = 1; j<5; j++){
+
+                matriz[i][j] = j*2;
+
+                System.out.print(matriz [i][j]+ " ")
+            }
+        }
+
+    }
+}
